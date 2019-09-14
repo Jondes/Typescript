1d609300a18f8c6549937edc9974787da79481de
johnson guedes <jondes560@hotmail.com>
2019-09-14 06:48:31 +0100

    Typescript
    Typescript

---
+++ ts-hello/main.ts
@@ -0,0 +1,21 @@
+/* function log(message) {
+    console.log(message);
+}
+
+var message = 'Hello world';
+
+log(message); */
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[]= [1,2,3];
+let f: any[]=[1, true, 'a', false]; 
+
+const ColorRed= 0;
+const ColorGreen= 1;
+const ColrBlue= 2;
+
+enum Color { Red =0, Green =1, Blue =2,Purple =3};
+let backgroundColor = Color.Red;
