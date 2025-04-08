f80d48905bc39d3a180d2816d8fa4451c661746c
Johnson Guedes <jondes560@hotmail.com>
2025-04-08 08:26:59 +0100

    Create App.tsx

---
+++ CodingPartner-React/StateManagement/state-management/src/App.tsx
@@ -0,0 +1,16 @@
+import Count from "./Count"
+import MyState from "./MyState"
+
+
+const App = () =>
+   {
+  return (
+    <div>
+      <MyState/>
+      <Count/>
+      
+      </div>
+  )
+}
+
+export default App
